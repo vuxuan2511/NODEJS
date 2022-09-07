0570c8b5c88b30e9fab7ff90335bd9e44e12a1cf
vuxuan2511 <vuxuan183@gmail.com>
2022-09-07 11:16:55 +0700

    test connect DB

---
+++ src/seeders/20220906143212-demo-user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        return queryInterface.bulkInsert('Users', [
+            {
+                firstName: 'John',
+                lastName: 'Doe',
+                email: 'example@example.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+    },
+
+    async down(queryInterface, Sequelize) {
+        return queryInterface.bulkDelete('Users', null, {});
+    },
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+};
