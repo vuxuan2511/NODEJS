cefffd76862c7bf07645d7ee7c22a9f3aa3330bd
vuxuan2511 <vuxuan183@gmail.com>
2022-09-09 15:56:04 +0700

    .

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import homeController from '../controller/homeController';
+import userController from '../controller/userController';
 
 //
 let router = express.Router();
@@ -13,6 +14,9 @@ let initWebRoutes = (app) => {
     router.get('/edit-crud', homeController.getEditCRUD);
     router.post('/put-crud', homeController.putCRUD);
     router.get('/delete-crud', homeController.deleteCRUD);
+
+    //API
+    router.post('/api/login', userController.handleLogin);
 
     return app.use('/', router);
 };
---
+++ src/services/userService.js
@@ -0,0 +1,64 @@
+import db from '../models/index';
+import bcrypt, { hash } from 'bcryptjs';
+
+//
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+            let isExist = await checkUserEmail(email);
+            if (isExist) {
+                //user already exist
+                let user = await db.User.findOne({
+                    where: { email: email },
+                    attributes: ['email', 'roleId', 'password'], //define  columns that you want to show
+                    raw: true,
+                });
+                if (user) {
+                    //compare password
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'ok';
+                        delete user.password;
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'wrong password';
+                    }
+                } else {
+                    userData.errCode = 2;
+                    userData.errMessage = `user isn't fould`;
+                }
+            } else {
+                //return error
+                userData.errCode = 1;
+                userData.errMessage = `your email in't exist in your system. pls try other email`;
+            }
+            resolve(userData);
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail },
+            });
+            if (user) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+module.exports = {
+    handleUserLogin: handleUserLogin,
+};
