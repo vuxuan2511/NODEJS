37371cb7859f2f9f57b066b8c94745225e184680
vuxuan2511 <vuxuan183@gmail.com>
2022-09-29 16:31:46 +0700

    doctor manage

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -152,15 +152,9 @@ let bulkCreateSchedule = (data) => {
                     attributes: ['timeType', 'date', 'doctorId', 'maxNumber'],
                     raw: true,
                 });
-                if (existing && existing.length > 0) {
-                    existing = existing.map((item) => {
-                        item.date = new Date(item.date).getTime();
-                        return item;
-                    });
-                }
 
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
                 if (toCreate && toCreate.length > 0) {
                     await db.Schedule.bulkCreate(toCreate);
@@ -190,6 +184,9 @@ let getScheduleByDate = (doctorId, date) => {
                         doctorId: doctorId,
                         date: date,
                     },
+                    include: [{ model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] }],
+                    raw: false,
+                    nest: true,
                 });
                 if (!dataSchedule) dataSchedule = [];
                 resolve({
